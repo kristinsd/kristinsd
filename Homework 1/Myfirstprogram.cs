29d7914ce667fc33068ba69f7baa517642c53734
kristinsd <kristin.d@abv.bg>
2015-10-19 19:25:21 +0300

    HW 2

---
+++ Homework 1/Myfirstprogram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello, World!;
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first program!"); 
+            Console.WriteLine("It took me just an hour!lol");
+            Console.Read();
+        }
+
+       
+    }
+}
---
+++ Homework 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first program!");
+            Console.WriteLine("It took me just an hour!lol");
+            Console.Read();
+        }
+
+
+    }
+    }
+
+ 
